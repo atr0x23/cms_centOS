d8ccdc2ddd2f8d94d9a2062ea23ea6c9dc9f947c
atr0x23 <atr0x23@gmail.com>
2021-01-16 18:28:07 +0200

    change the appearence position of comments

--- post.php
+++ post.php
@@ -124,12 +124,42 @@
                     
                 } else{}
     ?>    <!-- END of Create Comments -->
+
+          <!-- START of Show Comments -->
+    <?php            
+          $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
+        $query .= "AND comment_status = 'approved' ";
+        $query .= "ORDER BY comment_id DESC ";
+        $select_comment_query = mysqli_query($connection, $query);
+            confirmQuery($select_comment_query);
+        
+        
+        while($row = mysqli_fetch_array($select_comment_query)){
+            
+        $comment_date = $row['comment_date'];
+        $comment_content = $row['comment_content'];
+        $comment_author = $row['comment_author'];      
+        $comment_time = $row['comment_time'];      
                 
-                
-                
-                
-                
-                
+        ?>
+
+                <!-- The Comment -->
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading"><?php echo $comment_author; ?>
+                            <small><?php echo"<strong style='color:green'>$comment_date</strong>"; ?> at <?php echo "<strong style='color:green'>$comment_time </strong>"; ?></small>
+                        </h4>
+                        <?php echo $comment_content; ?>
+                    </div>
+                </div>    
+        <?php } ?>        
+                     
+          <!-- END of Show Comments --> 
+
+
 
                 <!-- Comments Form -->
                 <div class="well">
@@ -158,39 +188,10 @@
 
                 <!-- Posted Comments -->
                 
-                
-        <?php 
+                     
         
-        $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
-        $query .= "AND comment_status = 'approved' ";
-        $query .= "ORDER BY comment_id DESC ";
-        $select_comment_query = mysqli_query($connection, $query);
-            confirmQuery($select_comment_query);
-        
-        
-        while($row = mysqli_fetch_array($select_comment_query)){
-            
-        $comment_date = $row['comment_date'];
-        $comment_content = $row['comment_content'];
-        $comment_author = $row['comment_author'];      
-        $comment_time = $row['comment_time'];      
                 
-        ?>
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading"><?php echo $comment_author; ?>
-                            <small><?php echo"<strong style='color:green'>$comment_date</strong>"; ?> at <?php echo "<strong style='color:green'>$comment_time </strong>"; ?></small>
-                        </h4>
-                        <?php echo $comment_content; ?>
-                    </div>
-                </div>
-                
-        <?php } } }else{ header("Location: index.php");} ?>        
+        <?php } }else{ header("Location: index.php");} ?>        
 
                
                         <!-- Nested Comment --
