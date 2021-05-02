cd609b268eb9b7fc597339c7450dad7d9bc0a218
atr0x23 <atr0x23@gmail.com>
2021-05-02 19:58:49 +0300

    change posts layout. Make them more readable

--- user_posts.php
+++ user_posts.php
@@ -50,21 +50,22 @@
                     
                     ?>    
                         
-                     
-                <h2><?php echo $post_title ?></h2>
-                <p class="lead">
-                    by <?php echo $post_user; ?> 
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
-                <hr>
-                <a href="post.php?p_id=<?php echo $post_id; ?>">
-                <img class="img-responsive" src="images/<?php echo $post_image ;?>" alt="">
-                </a>
-                <hr>
-                <p><?php echo $post_content ?></p>
-               <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <div class="postboarders"> 
+                        <h2><?php echo $post_title ?></h2>
+                        <p class="lead">
+                            by <?php echo $post_user; ?> 
+                        </p>
+                        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                        <hr>
+                        <a href="post.php?p_id=<?php echo $post_id; ?>">
+                        <img class="img-responsive" src="images/<?php echo $post_image ;?>" alt="">
+                        </a>
+                        <hr>
+                        <p><?php echo $post_content ?></p>
+                        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <hr>
+                        <hr>
+                    </div>    
 
           <?php } ?>
 
